24f08b6ee17b2e2554592eb3aa5fdfc763050394
Juah3h32 <coronapablo99@gmail.com>
2026-05-25 00:19:10 -0600

    chore: bump version to 1.2.2

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 APP_NAME = "Farmacia Eben-Ezer"
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 PHARMACY_NAME = "FARMACIA EBEN-EZER"
 PHARMACY_ADDRESS = "DirecciÃ³n de la farmacia"
 PHARMACY_PHONE = "Tel: 000-000-0000"
